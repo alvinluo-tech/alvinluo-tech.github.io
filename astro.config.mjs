21491eec74618dc3c1898a37b26284b82d48aeeb
Alvin <luoyaosheng123@gmail.com>
2025-10-29 20:24:22 +0000

    docs:update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,8 +29,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://alvinluo-tech.github.io",
-
-	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
